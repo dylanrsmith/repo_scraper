9b2b1031da6a3722b323f094b319495d57092aff
Dylan Smith <dsmith@fargoengineering.com>
2022-11-23 11:23:25 -0600

    get config added

--- test.py
+++ test.py
@@ -23,53 +23,51 @@ import os
 def get_config():
     lines = []
     options = []
-
+    count = 0
+    repo_names = []
+    staging_spots = []
+    final_spots = []
     f = open("config.txt")
-    # for word in f.read().split('\r'):
-    #     # print(word)
-    #     lines.append(word)
     lines = [word for word in f.read().split("\r")]
 
-    for parameter in lines:
-        # for word in parameter.split():
-        #     options.append(word)
-        options = [word for word in parameter.split()]
+    for line in lines:
+        options = [word for word in line.split()]
 
-    for i in options:
-        print(i)
+    for option in options:
+        count+=1
+        if count == 1:
+            repo_names.append(option)
+        elif count == 2:
+            staging_spots.append(option)
+        elif count == 3:
+            final_spots.append(option)
+            count = 0
 
-
-# def get_files():
-import os
-
-files_arr = []
-dir_arr = []
-for root, dirs, files in os.walk("D:/repos/~test", topdown=False):
-    for name in files:
-        files_arr.append(os.path.join(root, name))
-    for name in dirs:
-        dir_arr.append(os.path.join(root, name))
-
-print(files_arr)
-print("------------")
-print(dir_arr)
+    print("REPOS:")
+    print(repo_names)
+    print("STAGES:")
+    print(staging_spots)
+    print("FINALS:")
+    print(final_spots)
 
 
-# for file in os.listdir(path_to):
-for file in files_arr:
-    if file.endswith(".py") or file.endswith(".txt"):  # Filter by file type
+def get_files():
+    files_arr = []
+    dir_arr = []
+    for root, dirs, files in os.walk("D:/repos/~test", topdown=False):
+        for name in files:
+            files_arr.append(os.path.join(root, name))
+        for name in dirs:
+            dir_arr.append(os.path.join(root, name))
+    for file in files_arr:
+        if file.endswith(".py") or file.endswith(".txt"):  # Filter by file type
 
-        # with open(filename, "r+") as f:                                             # Open file and write text
-        with open(file, "r+") as f:
-            content = f.read()
-            f.seek(0, 0)
-            # f.write(line.rstrip("\r\n") + "\n" + content)                           # line is content to write
-            f.write("LICENSE#32039805948" + "\n" + content)
-
-
-# get_config()
-
-
+            # with open(filename, "r+") as f:                                             # Open file and write text
+            with open(file, "r+") as f:
+                content = f.read()
+                f.seek(0, 0)
+                # f.write(line.rstrip("\r\n") + "\n" + content)                           # line is content to write
+                f.write("LICENSE#32039805948" + "\n" + content)
 def write_license():
     files_arr = []
     for root, dirs, files in os.walk("D:/repos/~test", topdown=False):
@@ -82,3 +80,6 @@ def write_license():
                 f.seek(0, 0)
                 # f.write(line.rstrip("\r\n") + "\n" + content)                           # line is content to write
                 f.write("LICENSE#32039805948" + "\n" + content)
+
+
+get_config()
